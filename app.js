a4562b16b1be82c8626478f7626ade3c3fef04d3
Salah Amer <Salah@example.com>
2019-10-09 18:32:21 +0200

    Trying with git in HTML_Resp

---
+++ app.js
@@ -0,0 +1,20 @@
+//jshint esversion:6
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/First Website_Resp.html");
+});
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server has started successfully.");
+});
+
+//if not working check naming it index.js here and in package.json
